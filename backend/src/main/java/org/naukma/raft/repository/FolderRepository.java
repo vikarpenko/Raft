d3116535c4960c48885efdb3a14599bdb7a1398d
Maryna Samsoniuk <m.samsonyuk@gmail.com>
2026-06-11 23:00:10 +0300

    fix(backend): fix note and folder repositories

--- backend/src/main/java/org/naukma/raft/repository/FolderRepository.java
+++ backend/src/main/java/org/naukma/raft/repository/FolderRepository.java
@@ -1,7 +1,23 @@
 package org.naukma.raft.repository;
 
 import org.naukma.raft.entity.Folder;
+import org.naukma.raft.enums.FolderType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    List<Folder> findByWorkspace_IdInOrderByCreatedDesc(Collection<Long> workspaceIds);
+
+    List<Folder> findByWorkspace_IdOrderByCreatedDesc(Long workspaceId);
+
+    List<Folder> findByWorkspace_IdAndTypeOrderByCreatedDesc(Long workspaceId, FolderType type);
+
+    boolean existsByWorkspace_IdAndName(Long workspaceId, String name);
+
+    Optional<Folder> findByIdAndWorkspace_Id(Long id, Long workspaceId);
+
+    void deleteByWorkspace_Id(Long workspaceId);
 }
--- backend/src/main/java/org/naukma/raft/repository/NoteRepository.java
+++ backend/src/main/java/org/naukma/raft/repository/NoteRepository.java
@@ -2,11 +2,30 @@ package org.naukma.raft.repository;
 
 import org.naukma.raft.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
-    List<Note> findByFolder_Id(Long folderId);
+    List<Note> findByFolder_IdOrderByUpdatedAtDesc(Long folderId);
 
-    void deleteByFolder_Id(Long workspaceId);
+    List<Note> findByFolder_IdInOrderByUpdatedAtDesc(Collection<Long> folderIds);
+
+    @Query("""
+        SELECT n FROM Note n
+        WHERE LOWER(n.title) LIKE LOWER(CONCAT('%', :query, '%'))
+           OR LOWER(n.content) LIKE LOWER(CONCAT('%', :query, '%'))
+        """)
+    List<Note> searchByTitleOrContent(@Param("query") String query, Pageable pageable);
+
+    List<Note> findByCreator_IdOrderByUpdatedAtDesc(Long creatorId);
+
+    List<Note> findByCreator_IdAndFolder_IdInOrderByUpdatedAtDesc(Long creatorId, Collection<Long> folderIds);
+
+    void deleteByFolder_Id(Long folderId);
+
+    long countByFolder_Id(Long folderId);
 }
