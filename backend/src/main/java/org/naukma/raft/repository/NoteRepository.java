dc7a560278a8e11b4c8238f7ac4c99aa92fa518a
Maryna Samsoniuk <m.samsonyuk@gmail.com>
2026-06-11 22:39:05 +0300

    fix(backend): fix note repository

--- backend/src/main/java/org/naukma/raft/repository/NoteRepository.java
+++ backend/src/main/java/org/naukma/raft/repository/NoteRepository.java
@@ -3,5 +3,10 @@ package org.naukma.raft.repository;
 import org.naukma.raft.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByFolder_Id(Long folderId);
+
+    void deleteByFolder_Id(Long workspaceId);
 }
